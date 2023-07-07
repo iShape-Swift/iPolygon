bd33cde748139b5874487552027262d1a10c1a1a
Nail Sharipov <nailxsharipov@gmail.com>
2023-07-07 16:18:19 +0300

    refactoring brute

--- Sources/iPolygon/Bool/Convex/Cross/CrossSolver+Brute.swift
+++ Sources/iPolygon/Bool/Convex/Cross/CrossSolver+Brute.swift
@@ -71,196 +71,37 @@ private struct Edge {
         guard self.bd.isCollide(other.bd) else {
             return .init(isCross: false, pin: .zero)
         }
-
-        let a0 = self.p0.point
-        let a1 = self.p1.point
-
-        let b0 = other.p0.point
-        let b1 = other.p1.point
         
-        let d0 = Triangle.clockDirection(p0: a0, p1: b0, p2: b1)
-        let d1 = Triangle.clockDirection(p0: a1, p1: b0, p2: b1)
-        let d2 = Triangle.clockDirection(p0: a0, p1: a1, p2: b0)
-        let d3 = Triangle.clockDirection(p0: a0, p1: a1, p2: b1)
+        let cross = ABEdge(e0: p0.point, e1: p1.point).cross(ABEdge(e0: other.p0.point, e1: other.p1.point))
+        
+        switch cross.type {
+        case .not_cross:
+            return CrossResult(isCross: false, pin: .zero)
+        case .pure:
+            let mA = MileStone(index: p0.index, offset: cross.point.sqrDistance(p0.point))
+            let mB = MileStone(index: other.p0.index, offset: cross.point.sqrDistance(other.p0.point))
 
-        if d0 == 0 || d1 == 0 || d2 == 0 || d3 == 0 {
-            if d0 == 0 && d1 == 0 && d2 == 0 && d3 == 0 {
-                return .init(isCross: false, pin: .zero)
-            }
-            if d0 == 0 {
-                if d2 == 0 || d3 == 0 {
-                    if d2 == 0 {
-                        // a0_b0
-                        return .init(
-                            isCross: true,
-                            pin: .init(
-                                p: a0,
-                                mA: p0.mileStone,
-                                mB: other.p0.mileStone
-                            ))
-                    } else {
-                        // a0_b1
-                        return .init(
-                            isCross: true,
-                            pin: .init(
-                                p: a0,
-                                mA: p0.mileStone,
-                                mB: other.p1.mileStone
-                            ))
-                    }
-                } else if d2 != d3 {
-                    // a0
-                    return .init(
-                        isCross: true,
-                        pin: .init(
-                            p: a0,
-                            mA: p0.mileStone,
-                            mB: other.p0.mileStone(point: a0)
-                        ))
-                } else {
-                    return .init(isCross: false, pin: .zero)
-                }
-            }
-            if d1 == 0 {
-                if d2 == 0 || d3 == 0 {
-                    if d2 == 0 {
-                        // a1_b0
-                        return .init(
-                            isCross: true,
-                            pin: .init(
-                                p: a1,
-                                mA: p1.mileStone,
-                                mB: other.p0.mileStone
-                            ))
-                    } else {
-                        // a1_b1
-                        return .init(
-                            isCross: true,
-                            pin: .init(
-                                p: a1,
-                                mA: p1.mileStone,
-                                mB: other.p1.mileStone
-                            ))
-                    }
-                } else if d2 != d3 {
-                    // a1
-                    return .init(
-                        isCross: true,
-                        pin: .init(
-                            p: a1,
-                            mA: p1.mileStone,
-                            mB: other.p0.mileStone(point: a1)
-                        ))
-                } else {
-                    return .init(isCross: false, pin: .zero)
-                }
-            }
-            if d0 != d1 {
-                if d2 == 0 {
-                    // b0
-                    return .init(
-                        isCross: true,
-                        pin: .init(
-                            p: b0,
-                            mA: p0.mileStone(point: b0),
-                            mB: other.p0.mileStone
-                        ))
-                } else {
-                    // b1
-                    return .init(
-                        isCross: true,
-                        pin: .init(
-                            p: b1,
-                            mA: p0.mileStone(point: b1),
-                            mB: other.p1.mileStone
-                        ))
-                }
-            } else {
-                return .init(isCross: false, pin: .zero)
-            }
-        } else if d0 != d1 && d2 != d3 {
-            let cross = Self.crossPoint(a0: a0, a1: a1, b0: b0, b1: b1)
-
-            // still can be ends
-            let isA0 = a0 == cross
-            let isA1 = a1 == cross
-            let isB0 = b0 == cross
-            let isB1 = b1 == cross
+            return CrossResult(isCross: true, pin: Pin(p: cross.point, mA: mA, mB: mB))
+        case .end_a:
+            let mA = MileStone(index: cross.point == p0.point ? p0.index : p1.index)
+            let mB = MileStone(index: other.p0.index, offset: cross.point.sqrDistance(other.p0.point))
             
-            let mA: MileStone
-            let mB: MileStone
-
-            if !(isA0 || isA1 || isB0 || isB1) {
-                mA = p0.mileStone(point: cross)
-                mB = other.p0.mileStone(point: cross)
-            } else if isA0 && isB0 {
-                // a0_b0
-                mA = p0.mileStone
-                mB = other.p0.mileStone
-            } else if isA0 && isB1 {
-                // a0_b1
-                mA = p0.mileStone
-                mB = other.p1.mileStone
-            } else if isA1 && isB0 {
-                // a1_b0
-                mA = p1.mileStone
-                mB = other.p0.mileStone
-            } else if isA1 && isB1 {
-                // a1_b1
-                mA = p1.mileStone
-                mB = other.p1.mileStone
-            } else if isA0 {
-                // a0
-                mA = p0.mileStone
-                mB = other.p0.mileStone(point: cross)
-            } else if isA1 {
-                // a1
-                mA = p1.mileStone
-                mB = other.p0.mileStone(point: cross)
-            } else if isB0 {
-                // b0
-                mA = p0.mileStone(point: cross)
-                mB = other.p0.mileStone
-            } else {
-                // b1
-                mA = p0.mileStone(point: cross)
-                mB = other.p1.mileStone
-            }
+            return CrossResult(isCross: true, pin: Pin(p: cross.point, mA: mA, mB: mB))
+        case .end_b:
+            let mA = MileStone(index: p0.index, offset: cross.point.sqrDistance(p0.point))
+            let mB = MileStone(index: cross.point == other.p0.point ? other.p0.index : other.p1.index)
             
-            return .init(
-                isCross: true,
-                pin: .init(
-                    p: cross,
-                    mA: mA,
-                    mB: mB
-                ))
-        } else {
-            return .init(isCross: false, pin: .zero)
+            return CrossResult(isCross: true, pin: Pin(p: cross.point, mA: mA, mB: mB))
+        case .common_end:
+            let mA = MileStone(index: cross.point == p0.point ? p0.index : p1.index)
+            let mB = MileStone(index: cross.point == other.p0.point ? other.p0.index : other.p1.index)
+            
+            return CrossResult(isCross: true, pin: Pin(p: cross.point, mA: mA, mB: mB))
         }
     }
-
-    private static func crossPoint(a0: FixVec, a1: FixVec, b0: FixVec, b1: FixVec) -> FixVec {
-        let dxA = a0.x - a1.x
-        let dyB = b0.y - b1.y
-        let dyA = a0.y - a1.y
-        let dxB = b0.x - b1.x
-
-        let xyA = a0.x * a1.y - a0.y * a1.x
-        let xyB = b0.x * b1.y - b0.y * b1.x
-        
-        let x = xyA * dxB - dxA * xyB
-        let y = xyA * dyB - dyA * xyB
-
-        let divider = dxA * dyB - dyA * dxB
-        
-        let cx = x / divider
-        let cy = y / divider
-        
-        return FixVec(cx, cy)
-    }
-    
 }
 
+ 
 private extension Array where Element == FixVec {
     
     func edges(filter: Boundary) -> [Edge] {
